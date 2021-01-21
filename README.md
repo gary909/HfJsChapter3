09290303f24596eaab5f7d1726b7fe432b138baa
gary909 <garywhite61@hotmail.com>
2021-01-21 15:25:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HfJsChapter3
---
+++ woofwoof.js
@@ -0,0 +1,11 @@
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name + " says WOOF WOOF");
+    } else {
+        console.log(name + " says Yip Yip");
+    }
+}
+bark("Rover", 23);
+bark("Spot", 13);
+bark("Spike", 53);
+bark("Lady", 17);
